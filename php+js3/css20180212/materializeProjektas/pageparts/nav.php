85899cb21f0e8bb26ee51f0abe17edd737484ece
Zigmantas Rackauskas <zigmantas@mrorangetech.se>
2018-02-12 22:13:19 +0200

    prideta

---
+++ php+js3/css20180212/materializeProjektas/pageparts/nav.php
@@ -0,0 +1,16 @@
+<nav>
+  <div class="nav-wrapper">
+    <a href="#!" class="brand-logo">Logo</a>
+    <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+    <ul class="right hide-on-med-and-down">
+      <li><a href="index.php">Pagrindinis</a></li>
+      <li><a href="login.php">Prisijungti</a></li>
+      <li><a href="about.php">Apie</a></li>
+    </ul>
+    <ul class="side-nav" id="mobile-demo">
+      <li><a href="index.php">Pagrindinis</a></li>
+      <li><a href="login.php">Prisijungti</a></li>
+      <li><a href="about.php">Apie</a></li>
+    </ul>
+  </div>
+</nav>
